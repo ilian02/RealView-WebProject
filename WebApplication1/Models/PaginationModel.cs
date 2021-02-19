18d5ead0410749a4007673700d2623d8ba7b2232
ilian <ileto02@abv.bg>
2021-02-19 18:32:59 +0200

    Added Painations

---
+++ WebApplication1/Models/PaginationModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebSite.Models
+{
+    public class PaginationModel
+    {
+
+        public List<PostModel> posts { get; set; }
+        public int size { get; set; }
+        public int index { get; set; }
+
+        public PaginationModel(List<PostModel> postModels, int size, int index)
+        {
+            this.posts = postModels;
+            this.size = size;
+            this.index = index;
+        }
+    }
+}
